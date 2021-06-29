a01d9c172f49fa24160c34e37b880233452cfeae
Germanvw99 <germanvazquez1999.dev@gmail.com>
2021-06-29 16:57:44 -0300

    1.1

---
+++ Inserts.sql
@@ -0,0 +1,46 @@
+﻿-- INSERTS PROVINCIAS
+BEGIN
+EXEC spAgregarProvincias 'Buenos Aires'
+EXEC spAgregarProvincias 'Capital Federal'
+EXEC spAgregarProvincias 'Catamarca'
+EXEC spAgregarProvincias 'Chaco'
+EXEC spAgregarProvincias 'Chubut'
+EXEC spAgregarProvincias 'Córdoba'
+EXEC spAgregarProvincias 'Corrientes'
+EXEC spAgregarProvincias 'Entre Ríos'
+EXEC spAgregarProvincias 'Formosa'
+EXEC spAgregarProvincias 'Jujuy'
+EXEC spAgregarProvincias 'La Pampa'
+EXEC spAgregarProvincias 'La Rioja'
+EXEC spAgregarProvincias 'Mendoza'
+EXEC spAgregarProvincias 'Misiones'
+EXEC spAgregarProvincias 'Neuquén'
+EXEC spAgregarProvincias 'Río Negro'
+EXEC spAgregarProvincias 'Salta'
+EXEC spAgregarProvincias 'San Juan'
+EXEC spAgregarProvincias 'San Luis'
+EXEC spAgregarProvincias 'Santa Cruz'
+EXEC spAgregarProvincias 'Santa Fe'
+EXEC spAgregarProvincias 'Santiago del Estero'
+EXEC spAgregarProvincias 'Tierra del Fuego'
+EXEC spAgregarProvincias 'Tucumán'
+END
+
+-- INSERTS ESTADOS
+BEGIN
+EXEC spAgregarEstados 'Baja'
+EXEC spAgregarEstados 'Alta'
+END
+
+-- INSERTS PERFILES
+BEGIN 
+EXEC spAgregarPerfiles 'Proveedores'
+EXEC spAgregarPerfiles 'Clientes'
+EXEC spAgregarPerfiles 'Personal'
+END
+-- INSERTS MEDIOS DE PAGO
+BEGIN
+EXEC spAgregarMedios_de_Pago 'Efectivo','Método de pago en efectivo'
+EXEC spAgregarMedios_de_Pago 'Débito','Método de pago a través de tarjeta de débito'
+EXEC spAgregarMedios_de_Pago 'Crédito','Método de pago a través de tarjeta de crédito'
+END
--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -1,1 +1,71 @@
-﻿
+﻿-- AGREGAR PROVINCIAS
+CREATE PROCEDURE spAgregarProvincias(
+@PROV_nombre VARCHAR(50)
+)
+AS
+	INSERT INTO Provincias(PROV_nombre)
+	VALUES(@PROV_nombre)
+GO
+
+CREATE PROCEDURE spEditarProvincias(
+@PROV_nombre VARCHAR(50),
+@PROV_codigo INT
+)
+AS
+UPDATE Provincias
+SET
+	PROV_nombre=@PROV_nombre
+WHERE
+	PROV_codigo= @PROV_codigo
+GO
+
+-- AGREGAR ESTADOS
+CREATE PROCEDURE spAgregarEstados(
+@EST_nombre VARCHAR(25)
+)
+AS
+	INSERT INTO Estados(EST_nombre)
+	VALUES(@EST_nombre)
+GO
+
+CREATE PROCEDURE spEditarEstados(
+@EST_nombre VARCHAR(50),
+@EST_codigo INT
+)
+AS
+UPDATE Estados
+SET
+	EST_nombre=@EST_nombre
+WHERE
+	EST_codigo= @EST_codigo
+GO
+
+-- AGREGAR PERFILES
+CREATE PROCEDURE spAgregarPerfiles(
+@PER_nombre VARCHAR(25)
+)
+AS
+	INSERT INTO Perfiles(PER_nombre)
+	VALUES(@PER_nombre)
+GO
+
+-- AGREGAR MEDIOS DE PAGO
+CREATE PROCEDURE spAgregarMedios_de_Pago(
+@MDP_nombre VARCHAR(50),
+@MDP_otros_detalles VARCHAR(100)
+)
+AS
+	INSERT INTO Medios_de_Pago(MDP_nombre,MDP_otros_detalles,MDP_estado_cod)
+	VALUES(@MDP_nombre,@MDP_otros_detalles,2)
+GO
+
+-- AGREGAR RECEPCION DE ARTICULO
+CREATE PROCEDURE spAgregarRecepcion_Articulos(
+@RECART_per_cod INT,
+@RECART_prov_cuit INT,
+@RECART_fecha SMALLDATETIME
+)
+AS 
+	INSERT INTO Recepcion_Articulos(RECART_per_cod,RECART_prov_cuit,RECART_fecha)
+	VALUES(@RECART_per_cod,@RECART_prov_cuit,@RECART_fecha)
+GO
