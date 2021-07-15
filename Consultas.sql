acc0835bdcb52c2785727c0b4a4a144ceca92176
arielst2021 <arieltapia42@gmail.com>
2021-07-15 11:48:21 -0300

    Create Consultas.sql

---
+++ Consultas.sql
@@ -0,0 +1,24 @@
+﻿--Consultas
+
+--los artículos mayores de una determinada cantidad en stock.
+
+SELECT ART_nombre AS 'Artículo',
+Articulos_por_Proveedor.AXP_stock_actual AS 'Stock'
+FROM Articulos_por_Proveedor
+INNER JOIN Articulos ON Articulos.ART_codigo = Articulos_por_Proveedor.AXP_codigo
+WHERE Articulos_por_Proveedor.AXP_stock_actual > 1000
+GO
+
+
+-- o con un procedimiento almacenado 
+CREATE PROCEDURE ArticulosMayorStock
+@stock INT
+AS
+SELECT ART_nombre AS 'Artículo',
+Articulos_por_Proveedor.AXP_stock_actual AS 'Stock'
+FROM Articulos_por_Proveedor
+INNER JOIN Articulos ON Articulos.ART_codigo = Articulos_por_Proveedor.AXP_codigo
+WHERE Articulos_por_Proveedor.AXP_stock_actual > @stock
+GO
+
+EXEC ArticulosMayorStock 1000
