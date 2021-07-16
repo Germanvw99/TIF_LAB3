2a800ed87b9a9a28a53cf330b9ae45360eff7cc7
arielst2021 <arieltapia42@gmail.com>
2021-07-16 19:39:00 -0300

    Update Consultas.sql

--- Consultas.sql
+++ Consultas.sql
@@ -58,3 +58,63 @@ GROUP BY art_nombre,DTV_articulo_cod,DTV_cantidad_unidades,DTV_precio_unitario
 GO
 
 EXEC DetalleVentaPorFactura 2
+
+-- CONSULTA QUE MUESTRA DATOS DEL PERSONAL
+SELECT
+USU_nombre AS 'Nombre',
+USU_apellido AS 'Apellido',
+USU_telefono AS 'Teléfono',
+USU_email AS 'eMail',
+USU_direccion AS 'Dirección',
+USU_ciudad AS 'Ciudad',
+PROV_nombre AS 'Provincia',
+EST_nombre AS 'Estado'
+FROM Usuarios
+INNER JOIN Provincias
+ON Provincias.PROV_codigo = Usuarios.USU_provincia_cod
+INNER JOIN Estados
+ON Estados.EST_codigo = Usuarios.USU_estado_cod
+INNER JOIN
+Perfiles
+ON Perfiles.PER_codigo = Usuarios.USU_perfil_cod
+WHERE Perfiles.PER_codigo = 1
+
+-- CONSULTA QUE MUESTRA DATOS DE LOS CLIENTES
+SELECT
+USU_nombre AS 'Nombre',
+USU_apellido AS 'Apellido',
+USU_telefono AS 'Teléfono',
+USU_email AS 'eMail',
+USU_direccion AS 'Dirección',
+USU_ciudad AS 'Ciudad',
+PROV_nombre AS 'Provincia',
+EST_nombre AS 'Estado'
+FROM Usuarios
+INNER JOIN Provincias
+ON Provincias.PROV_codigo = Usuarios.USU_provincia_cod
+INNER JOIN Estados
+ON Estados.EST_codigo = Usuarios.USU_estado_cod
+INNER JOIN
+Perfiles
+ON Perfiles.PER_codigo = Usuarios.USU_perfil_cod
+WHERE Perfiles.PER_codigo = 2
+
+-- CONSULTA QUE MUESTRA DATOS DE LOS PROVEEDORES
+SELECT
+USU_nombre AS 'Nombre',
+USU_apellido AS 'Apellido',
+USU_telefono AS 'Teléfono',
+USU_email AS 'eMail',
+USU_direccion AS 'Dirección',
+USU_ciudad AS 'Ciudad',
+PROV_nombre AS 'Provincia',
+EST_nombre AS 'Estado'
+FROM Usuarios
+INNER JOIN Provincias
+ON Provincias.PROV_codigo = Usuarios.USU_provincia_cod
+INNER JOIN Estados
+ON Estados.EST_codigo = Usuarios.USU_estado_cod
+INNER JOIN
+Perfiles
+ON Perfiles.PER_codigo = Usuarios.USU_perfil_cod
+WHERE Perfiles.PER_codigo = 3
