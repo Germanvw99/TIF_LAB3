ae19aaad1165f12cc57f85eb104542edd471beae
ckrooz66 <64717079+ckrooz66@users.noreply.github.com>
2021-07-16 10:20:34 -0300

    Se agrega consulta

--- Consultas.sql
+++ Consultas.sql
@@ -1,4 +1,4 @@
-﻿--Consultas
+--Consultas
 
 --los artículos mayores de una determinada cantidad en stock.
 
@@ -22,22 +22,10 @@ WHERE Articulos_por_Proveedor.AXP_stock_actual > @stock
 GO
 
 EXEC ArticulosMayorStock 1000
-GO
 
 
---CANTIDAD DE VENTAS DETALLADAS POR VENTA , CANTIDAD DE ARTÍCULOS COMPRADOS Y MONTO..
+--MOSTRAR CANTIDAD DE VENTAS REALIZADAS POR CUIT DE CLIENTE
+select distinct VEN_cli_cuit AS Cuit_de_cliente,COUNT(*) AS Cantidad_de_ventas_asociadas from Ventas
+group by VEN_cli_cuit
 
-SELECT DTV_venta_codigo AS [VENTA] ,
-COUNT(*) AS [Cantidad de Compras],
-SUM(DTV_cantidad_unidades) AS [Total de Unidades], 
-SUM(DTV_precio_unitario*DTV_cantidad_unidades) AS [MONTO]
-FROM DetalleVenta GROUP BY DTV_venta_codigo
-GO
 
---CANTIDAD DE UNIDADES VENDIDAS DE CADA ARTÍCULO, ORDENADAS DE MAYOR A MENOR ( MÁS DEMANDADA A LA MENOS DEMANDADA)
-
-SELECT AXP_articulo_cod AS [CÓDIGO],A.ART_nombre AS [NOMBRE], AXP_salida AS [CANTIDAD VENDIDA] FROM Articulos_por_Proveedor AS AP INNER JOIN Articulos AS A
-ON A.ART_codigo=AP.AXP_articulo_cod
-ORDER BY AXP_salida DESC
-
-GO
