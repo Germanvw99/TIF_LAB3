58f1e2b923685162b2c51f88b23db3059b8117d0
Cesar Ignacio <aquinocesarignacio@gmail.com>
2021-07-15 23:04:55 -0300

    Se agrega nueva consulta

--- Consultas.sql
+++ Consultas.sql
@@ -22,3 +22,14 @@ WHERE Articulos_por_Proveedor.AXP_stock_actual > @stock
 GO
 
 EXEC ArticulosMayorStock 1000
+GO
+
+
+--CANTIDAD DE VENTAS DETALLADAS POR VENTA , CANTIDAD DE ARTÍCULOS COMPRADOS Y MONTO..
+
+SELECT DTV_venta_codigo AS [VENTA] ,
+COUNT(*) AS [Cantidad de Compras],
+SUM(DTV_cantidad_unidades) AS [Total de Unidades], 
+SUM(DTV_precio_unitario*DTV_cantidad_unidades) AS [MONTO]
+FROM DetalleVenta GROUP BY DTV_venta_codigo
+GO
