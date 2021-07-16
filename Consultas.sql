5201800c95c59769cde0b60bf3fada337107b2e4
Cesar Ignacio <aquinocesarignacio@gmail.com>
2021-07-15 23:59:37 -0300

    Nueva consulta

--- Consultas.sql
+++ Consultas.sql
@@ -33,3 +33,11 @@ SUM(DTV_cantidad_unidades) AS [Total de Unidades],
 SUM(DTV_precio_unitario*DTV_cantidad_unidades) AS [MONTO]
 FROM DetalleVenta GROUP BY DTV_venta_codigo
 GO
+
+--CANTIDAD DE UNIDADES VENDIDAS DE CADA ARTÍCULO, ORDENADAS DE MAYOR A MENOR ( MÁS DEMANDADA A LA MENOS DEMANDADA)
+
+SELECT AXP_articulo_cod AS [CÓDIGO],A.ART_nombre AS [NOMBRE], AXP_salida AS [CANTIDAD VENDIDA] FROM Articulos_por_Proveedor AS AP INNER JOIN Articulos AS A
+ON A.ART_codigo=AP.AXP_articulo_cod
+ORDER BY AXP_salida DESC
+
+GO
